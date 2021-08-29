4220aada2ee5c42ad3979a3556891dde26214dcb
Carlos Barros <carlos_dbs@hotmail.com>
2021-08-29 19:24:06 -0300

    Alterando a chaves para parametros para retornar a promise

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -10,11 +10,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('1 - O retorno do telefonema', () => {
-  test('atende', () => {
+  test('atende', () => (
     answerPhone('teste').then((response) => {
       expect(response).toBe('Oi!');
-    });
-  });
+    })
+  ));
   test('ocupado', () => {
 
     // Insira seu teste assíncrono aqui
