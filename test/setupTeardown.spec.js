eebc14b10d38aa726b26fc5f5b4fb9839d01ab6a
Carlos Barros <carlos_dbs@hotmail.com>
2021-08-29 23:02:57 -0300

    Refatorando o console.log() para um afterEach()

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -25,24 +25,23 @@ describe('4 - Quem sobreviveu?', () => {
     randomAttack();
   });
 
+  afterEach(() => {
+    console.log(specialists);
+  });
+
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
-    console.log(specialists);
   });
   test('depois da segunda aventura', () => {
     expect(adventure.specialists.length).toBe(4);
-    console.log(specialists);
   });
   test('depois da terceira aventura', () => {
     expect(adventure.specialists.length).toBe(3);
-    console.log(specialists);
   });
   test('depois da quarta aventura', () => {
     expect(adventure.specialists.length).toBe(2);
-    console.log(specialists);
   });
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
-    console.log(specialists);
   });
 });
