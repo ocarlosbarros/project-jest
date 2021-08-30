c951936f91723ac8da8f590ccdb8e5cf4045c0d0
Carlos Barros <carlos_dbs@hotmail.com>
2021-08-30 03:38:05 -0300

    Adiciona fetchURL com spyOn

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,8 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('2 - Verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  const fetchURL = jest.spyOn(api, 'fetchURL');
+  afterEach(fetchURL.mockReset);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
